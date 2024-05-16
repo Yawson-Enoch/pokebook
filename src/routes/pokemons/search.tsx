28d636f6a585cbfe452db7e9f78a0735e5d8f290
Yawson-Enoch <enochgybex25@gmail.com>
2024-05-16 16:35:03 +0000

    fix: filter value autoreset

--- src/routes/pokemons/search.tsx
+++ src/routes/pokemons/search.tsx
@@ -10,15 +10,17 @@ export default function Search() {
   const [searchValue, setSearchValue] = React.useState(filter.s);
 
   /* set state in effect to allow trimming before setting filter in the url
-  - doing it directly on the input event handler prevents typing as it continously trims on value change
+  - doing it directly on the input event handler prevents typing for spaced out content "like this" as it continously trims on value change
+  - will not be allowed to have anything after "like" in this case
+  - not needed here, but it is good to have and also do not want to save multiple space values "      " in the url
+  - you can alternatively set to spaced values and trim on usage
    */
   React.useEffect(() => {
-    if (searchValue.trim() === '') {
-      setFilter({ s: undefined });
-    } else {
-      setFilter({ s: searchValue.trim() });
+    if (searchValue.trim() !== '') {
+      setFilter({
+        s: searchValue.trim(),
+      });
     }
-    setFilter({ page: undefined, pageOffset: undefined });
   }, [searchValue, setFilter]);
 
   return (
@@ -31,7 +33,17 @@ export default function Search() {
         placeholder="Enter pokemon name"
         className="w-full rounded-full bg-transparent px-2 font-sans caret-accent outline-none placeholder:text-[#7B7B7B] md:px-3 md:text-lg"
         value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={(e) => {
+          setFilter({
+            /* s: e.target.value.trim(),
+           - removed so you can have spaced out text
+           - trimming was done to remove all spacebar only value 
+             */
+            page: undefined,
+            pageOffset: undefined,
+          });
+          setSearchValue(e.target.value);
+        }}
       />
       <button
         type="button"
@@ -39,7 +51,10 @@ export default function Search() {
           'pointer-events-none invisible flex size-6 items-center justify-center self-center rounded-full bg-muted text-muted-foreground md:size-8',
           searchValue !== '' && 'pointer-events-auto visible',
         )}
-        onClick={() => setSearchValue('')}
+        onClick={() => {
+          setFilter({ s: undefined, page: undefined, pageOffset: undefined });
+          setSearchValue('');
+        }}
       >
         <Cross1Icon className="size-1/2" />
       </button>
